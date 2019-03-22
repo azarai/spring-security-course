1afb1f6f72f59f5106e3ad99a0176c0ea8fa146a
azarai <azarai@codeboje.de>
2019-03-22 11:44:33 +0100

    added spring session sample

--- src/main/java/de/codeboje/courses/springsecurity/WebSecurityConfiguration.java
+++ src/main/java/de/codeboje/courses/springsecurity/WebSecurityConfiguration.java
@@ -9,8 +9,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 
 import de.codeboje.courses.springsecurity.auth.AppUserDetailsService;
 
@@ -51,5 +54,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	PasswordEncoder passwordEncode() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public HttpSessionIdResolver httpSessionIdResolver() {
+		return HeaderHttpSessionIdResolver.xAuthToken(); 
 	}
 }
